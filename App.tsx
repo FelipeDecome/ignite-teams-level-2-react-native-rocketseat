d10e8ae954c7ec7cf357be9f5709787b24f19885
Felipe Decome <decome.felipe@gmail.com>
2024-07-15 18:15:06 -0300

    feat: install fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { ThemeProvider } from 'styled-components/native';
+
+import { Loading } from '@components/Loading';
 import { Groups } from '@screens/Groups';
 import { theme } from '@theme/index';
 
 export default function App() {
+  const [isLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {isLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
